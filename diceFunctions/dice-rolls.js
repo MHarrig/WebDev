63204544a9abeb53362e21d7273902b359b3948d
Mark <mark_harrigan@outlook.com>
2021-05-09 17:52:42 -0400

    Figuring out the DOM!

--- diceFunctions/dice-rolls.js
+++ diceFunctions/dice-rolls.js
@@ -8,6 +8,18 @@ diceButton.addEventListener('click', rollDice)
 }
 const dieTypes = [ 4, 6, 8, 10, 12, 20, 100 ];
 
+
+var ul = document.createElement('ul');
+ul.innerHTML = "Dice List";
+ul.id = 'diceList'
+document.querySelector('body').append(ul);
+
+
+
 for (const dieType of dieTypes) {
-  document.appendChild('diceList');
+  var btn = document.createElement('button');
+  var li = document.createElement('li');
+  btn.innerHTML = "Dice";
+  ul.append(li);
+  li.append(btn);
 }
