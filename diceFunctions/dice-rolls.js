bb4fca1b990276667e15b63b9fb9ced7311ba4f7
Mark Harrigan <53972996+MHarrig@users.noreply.github.com>
2021-05-09 13:04:47 -0400

    Experimenting with the JS button maker

--- diceFunctions/dice-rolls.js
+++ diceFunctions/dice-rolls.js
@@ -1,8 +1,13 @@
 
 let diceButton = document.getElementById('diceRoll');
 
-diceButton.addEventListener('onclick', rollDice)
+diceButton.addEventListener('click', rollDice)
   function rollDice() {
     let roll = Math.floor(Math.random() * 6) +1;
     alert(roll);
+}
+const dieTypes = [ 4, 6, 8, 10, 12, 20, 100 ];
+
+for (const dieType of dieTypes) {
+  document.appendChild('diceList');
 }
