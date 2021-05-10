9d8ec799ddb85bb36d6d64985c8a8931432dfd7a
Mark Harrigan <53972996+MHarrig@users.noreply.github.com>
2021-05-10 19:34:29 -0400

    I finished the fundamental DiceFunction. There are now buttons for each of the basic Dice Types, and clicking them will produce an alert with the dice roll result. And each button will only produce a result within the limit of the dice selected.

--- diceFunctions/dice-rolls.js
+++ diceFunctions/dice-rolls.js
@@ -1,11 +1,6 @@
 
 let diceButton = document.getElementById('diceRoll');
 
-diceButton.addEventListener('click', rollDice)
-  function rollDice() {
-    let roll = Math.floor(Math.random() * 6) +1;
-    alert(roll);
-}
 const dieTypes = [ 4, 6, 8, 10, 12, 20, 100 ];
 
 
@@ -19,7 +14,14 @@ document.querySelector('body').append(ul);
 for (const dieType of dieTypes) {
   var btn = document.createElement('button');
   var li = document.createElement('li');
-  btn.innerHTML = "Dice";
+  btn.innerHTML = "D" + `${dieType}`;
+  btn.value = dieType;
   ul.append(li);
   li.append(btn);
+  btn.addEventListener('click', rollDice)
+    function rollDice(dieTypes) {
+      let roll = Math.floor(Math.random() * dieType) +1;
+      alert(roll);
+  }
+
 }
