423e83fa4a67f1f2980d05dac19536824453da1c
Mark <mark_harrigan@outlook.com>
2021-05-12 21:34:18 -0400

    Started work on the advantage function integration

--- diceFunctions/dice-rolls.js
+++ diceFunctions/dice-rolls.js
@@ -10,6 +10,9 @@ ul.id = 'diceList'
 document.querySelector('body').append(ul);
 
 
+//This is the function that dictates the dice rolling buttons.
+//It gives the buttons their values of the dice and sets them so that they can only roll within the possible values of the dice.
+// I will need to try and separate out the dice rolling function so it can be a global varibale which can be accessible to the advantage and disadvantage functions.
 
 for (const dieType of dieTypes) {
   var btn = document.createElement('button');
@@ -25,3 +28,20 @@ for (const dieType of dieTypes) {
   }
 
 }
+
+//So far this is the advantage function, will add in a disadvantage function that can do the inverse.
+
+var advantage = document.createElement('button');
+advantage.innerHTML = "With Advantage";
+document.querySelector('#diceRoll').append(advantage);
+advantage.addEventListener('click', withAdvantage)
+  function withAdvantage() {
+    let roll1 = Math.floor(Math.random() * 6) +1;
+    let roll2 = Math.floor(Math.random() * 6) +1;
+    if(roll1 > roll2){
+      alert(roll1);
+    } else if (roll2 > roll1) {
+      alert(roll2);
+    }
+    console.log(roll1, roll2);
+  }
